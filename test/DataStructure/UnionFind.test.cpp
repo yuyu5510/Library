0f15b8c8715a3fe2bde9a125be76a05d823915a7
yuyu5510 <yuyu5510.chara@icloud.com>
2023-11-11 18:28:36 +0900

    add UnionFind

---
+++ test/DataStructure/UnionFind.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM https:// judge.yosupo.jp/problem/unionfind
+
+#include <iostream>
+#include "../../src/DataStructure/Unionfind.hpp"
+
+int main() {
+    int N, Q;
+    std::cin >> N >> Q;
+    lib::UnionFind uf(N);
+    for (int i = 0; i < Q; i++) {
+        int t, u, v;
+        std::cin >> t >> u >> v;
+        if (t == 0) {
+            uf.unite(u, v);
+        } else {
+            std::cout << uf.same(u, v) << '\n';
+        }
+    }
+}
