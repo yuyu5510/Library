4685d25e121ab7a02b34b0cfe5e569b91559e941
yuyu5510 <yuyu5510.chara@icloud.com>
2023-11-11 19:47:23 +0900

    fix url

--- test/DataStructure/UnionFind.test.cpp
+++ test/DataStructure/UnionFind.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https:// judge.yosupo.jp/problem/unionfind"
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 
 #include <iostream>
 #include "../../src/DataStructure/UnionFind.hpp"
